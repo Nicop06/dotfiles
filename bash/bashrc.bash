456316db366548485d40947ffdc7c0887fe206a5
Nicolas Porcel <nicolasporcel06@gmail.com>
2014-06-23 01:29:27 +0800

    Colored man pages

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -49,3 +49,16 @@ export EDITOR='/usr/bin/vim'
 
 # Bash-completion
 [ -r /usr/share/bash-completion/bash_completion ] && . /usr/share/bash-completion/bash_completion
+
+# Colored man
+man() {
+ env \
+  LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+  LESS_TERMCAP_md=$(printf "\e[1;31m") \
+  LESS_TERMCAP_me=$(printf "\e[0m") \
+  LESS_TERMCAP_se=$(printf "\e[0m") \
+  LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+  LESS_TERMCAP_ue=$(printf "\e[0m") \
+  LESS_TERMCAP_us=$(printf "\e[1;32m") \
+   man "$@"
+}
