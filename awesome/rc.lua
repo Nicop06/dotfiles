9d58c0e9b4142812cf66401edb55e32a7b2fb441
Nicolas Porcel <nicolasporcel06@gmail.com>
2015-09-13 15:46:40 +0200

    Revert "Switch from urxvtc to urxvt"
    This reverts commit 3e6fe9ed3c46414da53aad43c18faa22e4c9619d.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -43,7 +43,7 @@ end
 beautiful.init(".config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "urxvtc"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
