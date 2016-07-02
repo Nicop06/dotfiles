3a7e8e81b90d4cfb0a3dac7226dc2b82b925b484
Nicolas Porcel <nicolasporcel06@gmail.com>
2016-07-02 22:34:16 +0200

    Add notification to touchpad script

--- bin/touchpadToggle.sh
+++ bin/touchpadToggle.sh
@@ -1,8 +1,14 @@
 #! /bin/sh
+
+NOTIFY=false
+[ "$1" = "-n" ] && NOTIFY=true
+
 pointer_status=$(synclient -l | grep TouchpadOff)
 	
 if [ -z `echo $pointer_status | grep -o 1` ]; then
-	synclient TouchpadOff = 1
+	synclient TouchpadOff=1
+  $NOTIFY && notify-send 'Touchpad' 'Disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 else
-	synclient TouchpadOff = 0
+	synclient TouchpadOff=0
+  $NOTIFY && notify-send 'Touchpad' 'Enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 fi
